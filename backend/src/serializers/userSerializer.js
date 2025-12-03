fdb3c9a50e6bcbb7cb92389960d95b62db143555
Thomas Bemus <bemutj01@pfw.edu>
2025-12-03 16:08:02 -0500

    Quick fix patch

--- backend/src/serializers/userSerializer.js
+++ backend/src/serializers/userSerializer.js
@@ -2,6 +2,7 @@ export const serializeUser = (user) => ({
   id: user.id,
   username: user.username,
   email: user.email,
+  image: user.image || '',
   about: user.about,
   favorites: user.favorites?.map((fav) =>
     typeof fav === "object" ? fav.toString() : fav
